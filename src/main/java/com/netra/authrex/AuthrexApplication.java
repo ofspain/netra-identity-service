67ab1914eded3d0447e6c2e42b8bb4bacadcf01e
FEMI <oluwafemi.ayeni@interswitchgroup.com>
2025-07-27 21:57:29 +0100

    bootstrapping project

---
+++ src/main/java/com/netra/authrex/AuthrexApplication.java
@@ -0,0 +1,13 @@
+package com.netra.authrex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthrexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthrexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netra/authrex/AuthrexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netra.authrex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthrexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
